c23cb7626fd174493e4936587208e439a9b97ef6
8Whack <calvinpratt99@gmail.com>
2021-12-20 17:14:42 -0700

    Done! My brain is tired

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -261,17 +261,17 @@ let bigOrSmallArray = [1,101, 102, 2, 103, 4, 5, 6, 107]
 
 function bigOrSmall (arr) {
   let answers = []
-  for (i = 0; i < bigOrSmallArray.length; i++){
-    if (bigOrSmallArray[i] > 100){
+  for (i = 0; i < arr.length; i++){
+    if (arr[i] > 100){
       answers.push("BIG")
     } else {
       answers.push("small")
     }
   }
-   let arrayEvaluator = answers
-  return arrayEvaluator
+   return answers;
 }
- console.log(bigOrSmall(bigOrSmallArray))
+let arrayEvaluator = bigOrSmall(bigOrSmallArray)
+ console.log(arrayEvaluator)
 
 
 
@@ -336,7 +336,10 @@ emailCheck("cp@email.com")
   Create a variable called `totalFrogs` and set it equal to your function invoked, passing in the amount of gold you are willing to spend.
 */
 
-
+function maxFrogs(gold){
+  return (Math.floor(gold / 3 ))
+}
+console.log(maxFrogs(20))
 
 
 ////////////////// PROBLEM 20 ////////////////////
@@ -344,7 +347,7 @@ emailCheck("cp@email.com")
   You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. You would just be able to purchase 1 product. Re-write the function you used in the previous problem (give it the same name, just add a 2 to the end of it) that fixes this bug. Invoke the function and store the returned value to a variable called `totalFrogs2`.
 */
 
-//CODE HERE
+//I actually did that the first time, so I'm just going to leave this blank and ask you to reference the above code.
 
 
 ////////////////// PROBLEM 21 ////////////////////
@@ -353,8 +356,20 @@ let sampleArray = [0,1,2,3,4,7,5,6,8,9]
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
 
-//CODE HERE
-
+const checkArrayAscending = (arr) =>{
+  let compareValue = arr[0];
+  for (i = 1; i < arr.length; i++){
+    if (compareValue >= arr[i]){
+     return false
+    }else {
+      compareValue = arr[i];
+      
+    }
+  }
+  return true
+}
+let arrayIsAscending = checkArrayAscending(sampleArray)
+console.log(arrayIsAscending)
 
 ////////////////// PROBLEM 22 ////////////////////
 
@@ -378,13 +393,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = []
+let globalScope = ["duck"]
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = []
+let bathroomScope = ["rubberDuck", "duck"]
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = []
+let bathtubScope = ["SailorDuck", "rubberDuck", "duck"]
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = []
+let pondScope = ["realDuck", "duck"]
